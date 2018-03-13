e30564883742db5bbb4806b8f4bc9d9fd2a3f901
David Sondak <dsondak@seas.harvard.edu>
2018-03-13 16:20:56 -0400

    Made `shared' partition the default in slurm script.

--- Inadequacy_Code/implicit_integrator/irreversible/kinetics_inference.sh
+++ Inadequacy_Code/implicit_integrator/irreversible/kinetics_inference.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -n 2 # Number of cores requested
 #SBATCH -N 1 # Ensure that all cores are on one machine
-#SBATCH -t 0-02:00 # Maximum execution time (D-HH:MM)
-#SBATCH -p general # Partition to submit to
+#SBATCH -t 0-00:05 # Maximum execution time (D-HH:MM)
+#SBATCH -p shared # Partition to submit to
 #SBATCH --mem=512 # Memory per cpu in MB (see also --mem-per-cpu)
 #SBATCH --open-mode=append
 #SBATCH -o hostname_%j.out # Standard out goes to this file
